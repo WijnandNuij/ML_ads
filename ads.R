96603829a1c3b8dddc59003f13a0745800f27d0e
Wijnand Nuij <wijnandnuij@gmail.com>
2014-06-29 11:02:13 +0200

    ads

--- ads.R
+++ ads.R
@@ -1,14 +1,22 @@
 # rm(list=ls(all=TRUE))
+# nrows train.csv 45.840.618
+# nrows test.csv 6.042.136
 
-loadData <- function(location='/home/wijnand/R_workspace_ads/resources/train.csv')
+loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.csv')
 {
-  require(data.table)
-  
-  # nrows train: 45.840.618
-  # nrows test: 6.042.136
-  columnClasses <- sapply(read.csv("/home/wijnand/R_workspace_ads/resources/1000rows.csv",nrows=1000),class)
-  numberOfRows <- 45840618
-  
-  rawData <- fread(input = location, sep = ",", header=T, stringsAsFactors = F, colClasses = columnClasses, 
-                   nrows = numberOfRows)
+        columnClasses <- sapply(read.csv("/home/wijnand/R_workspace_ads/resources/1000rows.csv",nrows=1000),class)
+        
+        require(data.table)
+        rawData <- fread(input = location, sep = ",", header=T)
+        
+        # convert all character columns to factors, due to stupid bug in data.table
+        for(i in 1:26)
+        {
+                columnName <- paste0("C", i)
+                rawData[[columnName]] <- as.factor(rawData[[columnName]])
+                rawData[[columnName]] <- as.numeric(rawData[[columnName]])
+                rawData[[columnName]] <- as.factor(rawData[[columnName]])
+        }
+        print(str(rawData))
+        rawData
 }
