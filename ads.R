1de0050fe57bef7a2f7c855a397b7ee8758d9d95
Wijnand Nuij <wijnandnuij@gmail.com>
2014-07-01 21:48:27 +0200

    filter on factor levels

--- ads.R
+++ ads.R
@@ -6,48 +6,63 @@ runTraining <- function(percentageTrain=0.7)
 {
         ads <- loadData()
         
-        
         # row number of x% of the data for training set
-        #numberTrainingSet <- round(nrow(ads) * percentageTrain, digits=0) - 1
-        #trainingSet <- ads[1:numberTrainingSet,]
-        #testSet <- ads[(numberTrainingSet+1):nrow(ads),]
+        numberTrainingSet <- round(nrow(ads) * percentageTrain, digits=0) - 1
+        trainingSet <- ads[1:numberTrainingSet,]
+        print(nrow(trainingSet))
+        testSet <- ads[(numberTrainingSet+1):nrow(ads),]
+        print(nrow(testSet))
         
-        require(caret)
-        trainIndex <- createDataPartition(ads$Label, p = 0.7, list=FALSE)
-        trainingSet <- ads[trainIndex, ]
-        testSet <- ads[-trainIndex, ]
-
-        #trainedModel <- m5tree(trainingSet)
-        trainedModel <- linearModel(trainingSet)
+        trainedModel <- m5tree(trainingSet)
+        #trainedModel <- linearModel(trainingSet)
         result <- predict(trainedModel, testSet)
         
         printPredictionResults(result, testSet$Label)
 }
 
-loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.csv', levelTreshHold = 20)
+loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.csv', numberOfValues=1000)
 {
         columnClasses <- sapply(read.csv("/home/wijnand/R_workspace_ads/resources/1000rows.csv",nrows=1000),class)
         
         require(data.table)
-        rawData <- fread(input = location, sep = ",", header=T)
+        rawData <- fread(input = location, sep = ",", header=T, na.strings = "")
+        
+        print(str(rawData))
         
         # convert all character columns to factors, due to stupid bug in data.table
         for(i in 1:26)
         {
                 columnName <- paste0("C", i)
                 rawData[[columnName]] <- as.factor(rawData[[columnName]])
-                rawData[[columnName]] <- as.numeric(rawData[[columnName]])
-                rawData[[columnName]] <- as.factor(rawData[[columnName]])
                 
-                # to start simple, remove columns with a lot of values
-                if(nlevels(rawData[[columnName]]) > levelTreshHold)
+                # select levels with more than x values
+                relevantLevels <- table(rawData[[columnName]]) >= numberOfValues
+                
+                # get their names
+                relevantLevels <- names(relevantLevels[relevantLevels==TRUE])
+                print(relevantLevels)
+                
+                # if not relevant relevant, NA
+                rawData[[columnName]] <- ifelse(rawData[[columnName]] %in% relevantLevels, rawData[[columnName]], NA)
+                rawData[[columnName]] <- as.factor(rawData[[columnName]])
+                print(nlevels(rawData[[columnName]]))
+                if(nlevels(rawData[[columnName]]) <= 1)
                 {
+                        
+                        # remove when no levels reach the treshhold
                         rawData[[columnName]] <- NULL
+                        print("removing column")
                 }
+                else
+                {
+                        print("keeping column")
+                }
+                
         }
+        #rawData$Id <- NULL
         
         # randomize
-        rawData <- rawData[order(runif(nrow(rawData))),]
+        #rawData <- rawData[order(runif(nrow(rawData))),]
         
         print(str(rawData))
         rawData
@@ -62,6 +77,7 @@ m5tree <- function(data)
 
 linearModel <- function(data)
 {
+        data <- na.omit(data)
         trainedModel <- lm(Label ~ . , data, na.action = na.omit)
 }
 
