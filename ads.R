e26929414ea51dc12685fd1fe0f85eb4cf80a8f5
WijnandNuij <wijnandnuij@gmail.com>
2014-07-03 15:41:54 +0200

    problems with training and testset

--- ads.R
+++ ads.R
@@ -2,51 +2,71 @@
 # nrows train.csv 45.840.618
 # nrows test.csv 6.042.136
 
-runTraining <- function(percentageTrain=0.7)
+runTraining <- function(percentageTrain=0.5)
 {
         ads <- loadData()
+        ads[["C9"]] <- NULL
+        ads[["C17"]] <- NULL
+        #print(str(ads))
         
         # row number of x% of the data for training set
         numberTrainingSet <- round(nrow(ads) * percentageTrain, digits=0) - 1
         trainingSet <- ads[1:numberTrainingSet,]
-        print(nrow(trainingSet))
         testSet <- ads[(numberTrainingSet+1):nrow(ads),]
-        print(nrow(testSet))
         
-        trainedModel <- m5tree(trainingSet)
+        print(str(trainingSet))
+        print(table(trainingSet$C9))
+        print(length(trainingSet$C9))
+        
+        print(str(testSet))
+        print(table(testSet$C9))
+        print(length(testSet$C9))
+        
+        #trainedModel <- m5tree(trainingSet)
         #trainedModel <- linearModel(trainingSet)
+        trainedModel <- generalizedLinearModel(trainingSet)
+        
+        print(summary(trainedModel))
         result <- predict(trainedModel, testSet)
+        result <- ifelse(result < 0, yes = 0, result)
+        result <- ifelse(result > 1, yes = 1, result)
+        
         
         printPredictionResults(result, testSet$Label)
 }
 
-loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.csv', numberOfValues=1000)
+loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.csv', numberOfValues=5000)
 {
         columnClasses <- sapply(read.csv("/home/wijnand/R_workspace_ads/resources/1000rows.csv",nrows=1000),class)
         
         require(data.table)
         rawData <- fread(input = location, sep = ",", header=T, na.strings = "")
         
-        print(str(rawData))
+        rawData$Id <- NULL
+        
+        # randomize
+        set.seed(123)
+        rawData <- rawData[order(runif(nrow(rawData))),]
         
         # convert all character columns to factors, due to stupid bug in data.table
         for(i in 1:26)
         {
                 columnName <- paste0("C", i)
-                rawData[[columnName]] <- as.factor(rawData[[columnName]])
+                #rawData[[columnName]] <- as.factor(rawData[[columnName]])
                 
                 # select levels with more than x values
                 relevantLevels <- table(rawData[[columnName]]) >= numberOfValues
                 
                 # get their names
                 relevantLevels <- names(relevantLevels[relevantLevels==TRUE])
-                print(relevantLevels)
+                #print(relevantLevels)
                 
                 # if not relevant relevant, NA
-                rawData[[columnName]] <- ifelse(rawData[[columnName]] %in% relevantLevels, rawData[[columnName]], NA)
-                rawData[[columnName]] <- as.factor(rawData[[columnName]])
+                rawData[[columnName]] <- ifelse(rawData[[columnName]] %in% relevantLevels, rawData[[columnName]], "no_value")
+                #rawData[[columnName]] <- ifelse(rawData[[columnName]] == "", rawData[[columnName]], "no_value")
+                rawData[[columnName]] <- factor(rawData[[columnName]])
                 print(nlevels(rawData[[columnName]]))
-                if(nlevels(rawData[[columnName]]) <= 1)
+                if(nlevels(rawData[[columnName]]) <= 2)
                 {
                         
                         # remove when no levels reach the treshhold
@@ -56,15 +76,10 @@ loadData <- function(location='/home/wijnand/R_workspace_ads/resources/500krows.
                 else
                 {
                         print("keeping column")
+                        #print(table(rawData[[columnName]]))
                 }
                 
         }
-        #rawData$Id <- NULL
-        
-        # randomize
-        #rawData <- rawData[order(runif(nrow(rawData))),]
-        
-        print(str(rawData))
         rawData
 }
 
@@ -77,17 +92,26 @@ m5tree <- function(data)
 
 linearModel <- function(data)
 {
-        data <- na.omit(data)
-        trainedModel <- lm(Label ~ . , data, na.action = na.omit)
+        print(str(data))
+        trainedModel <- lm(Label ~ . , data)
+}
+
+generalizedLinearModel <- function(data)
+{
+        print(str(data))
+        trainedModel <- glm(Label ~ . , data, family = binomial(logit))
 }
 
 printPredictionResults <- function(prediction, actual)
 {
+        print(summary(prediction))
+        print(summary(actual))
         #require(gmodels)
-        #table <- CrossTable(prediction, testset$Label, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, 
+        #table <- CrossTable(prediction, actual, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, 
         #                    prop.t = TRUE, dnn=(c('actual result', 'predicted result')))
         #precision <- (table$prop.tbl[1,1] + table$prop.tbl[2,2]) * 100
         #print(paste0("Precision as percentage: ", round(precision, digits=3)))
+        
         
         # Logarithmic loss - kaggle-method for score calc
         epsilon <- .000000000000001
